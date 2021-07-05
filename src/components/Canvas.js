110b0409c533505d9c4eeb6ad565b956f8d8f131
gaurav-sharma-snaphunt <gaurav.sharma@snaphunt.com>
2021-07-05 14:41:58 +0800

    ⚡️Add symmetric board

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,4 +1,4 @@
-const input = require('./Input');
+const input = require("./Input");
 
 class Canvas {
   constructor() {
@@ -9,12 +9,12 @@ class Canvas {
   getCellValue = (colIndex, rowIndex, w, h) => {
     const result =
       rowIndex === 0 || rowIndex === h - 1
-        ? '-'
+        ? "-"
         : colIndex === 0 || colIndex === w - 1
-        ? '|'
-        : ' ';
+        ? "|"
+        : " ";
     console.log(
-      'rowIndex, colIndex, w, h, result',
+      "rowIndex, colIndex, w, h, result",
       rowIndex,
       colIndex,
       w,
@@ -26,7 +26,7 @@ class Canvas {
 
   createBoard = (w, h) => {
     if (!w || !h) return;
-    if (typeof w !== 'number' || typeof h !== 'number') return;
+    if (typeof w !== "number" || typeof h !== "number") return;
 
     const borderedWidth = w + 2;
     const borderedHeight = h + 2;
@@ -46,7 +46,7 @@ class Canvas {
 
   draw() {
     this.board.map((row) => {
-      let renderCells = '';
+      let renderCells = "";
       row.map((cell) => {
         renderCells += cell.value;
       });
@@ -59,12 +59,12 @@ class Canvas {
     // const size = await input(
     //   'Welcome, please enter size to create your canvas: '
     // );
-    const size = 'C 3 2';
-    console.log('\n');
+    const size = "C 3 2";
+    console.log("\n");
 
-    const [commandType, width, height] = (size && size.split(' ')) || [];
+    const [commandType, width, height] = (size && size.split(" ")) || [];
 
-    if (commandType !== 'C' || !width || !height) {
+    if (commandType !== "C" || !width || !height) {
       // TODO: validate number
       console.log("Please enter size of canvas in format 'C width height'\n");
       this.initialise();
@@ -75,6 +75,7 @@ class Canvas {
     console.log(this.draw());
   }
 
+  // Make old canvas data eligible for garbage collection
   clear() {
     this.board = [];
   }
